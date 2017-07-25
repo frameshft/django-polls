00f28a593ac23f072ab7d770a9f1f830f24e3932
frameshft <adiletkadyrov@gmail.com>
2017-07-25 16:28:24 +0600

    Part 5

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
@@ -24,6 +25,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.choice_text
